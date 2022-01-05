2659484506a0260c6c6f4dab0c2f605acd6d581a
marinajulia <marinajulia707@gmail.com>
2022-01-05 15:38:46 -0300

    refactor dto phone

--- PhoneBook.Domain/Services/Phone/Dto/PhoneDto.cs
+++ PhoneBook.Domain/Services/Phone/Dto/PhoneDto.cs
@@ -1,6 +1,4 @@
-﻿using PhoneBook.Domain.Services.Contact;
-
-namespace PhoneBook.Domain.Services.Phone.Dto
+﻿namespace PhoneBook.Domain.Services.Phone.Dto
 {
     public class PhoneDto
     {
@@ -11,8 +9,6 @@ namespace PhoneBook.Domain.Services.Phone.Dto
         }
 
         public int Id { get; set; }
-        public int IdContact { get; set; }
         public string Phone { get; set; }
-        public ContactEntity Contact { get; set; }
     }
 }
